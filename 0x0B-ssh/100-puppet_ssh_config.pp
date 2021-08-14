2b1b092d1f089980e9d3bc2d2f0e41bdd9027b9a
jeanpierreba <2713@holbertonschool.com>
2021-08-14 13:00:48 -0500

    Added pupper file to configurate ssh file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# sets up a client SSH configuration file
+include stdlib
+file_line { 'Declare identity file':
+    path    => '/etc/ssh/ssh_config',
+    line    => '	IdentityFile ~/.ssh/holberton',
+    replace => true,
+}
+file_line { 'Turn off passwd auth':
+    path    => '/etc/ssh/ssh_config',
+    line    => '   PasswordAuthentication no',
+    replace => true,
+}
