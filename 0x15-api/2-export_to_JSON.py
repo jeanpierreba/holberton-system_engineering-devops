3d73632bb499f65765e07785a376b50c0ee541b1
jeanpierreba <2713@holbertonschool.com>
2021-09-13 17:32:31 -0500

    moved imports inside if

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """ Exports file 0 result into json file """
 
-import json
-import requests
-import sys
-
-
 if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
     user_id = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(user_id))
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     todos = todos.json()
+
     todos_user = {}
     tasks_list = []
 
     for task in todos:
-        if task.get("userId") == int(user_id):
-            task_dict = {"task": task.get("title"),
-                         "completed": task.get('completed'),
-                         "username": user.json().get("username")}
-            tasks_list.append(task_dict)
+        if task.get('userId') == int(user_id):
+            taskDict = {"task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": user.json().get('username')}
+            tasks_list.append(taskDict)
     todos_user[user_id] = tasks_list
 
-    file_name = user_id + '.json'
-    with open(file_name, 'w') as file:
+    filename = user_id + '.json'
+    with open(filename, mode='w') as file:
         json.dump(todos_user, file)
