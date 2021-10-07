39749f9e0c5e1c2f053ed4175308b10c4808c139
jeanpierreba <2713@holbertonschool.com>
2021-10-07 10:33:22 -0500

    Added sudo to command

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,7 @@
 # Make possible to login with holberton user
 exec { 'change-os-configuration-for-holberton-user':
-  environment => ['DIR=/etc/security/limits.conf',
-                  'OLD=hard nofile 5',
-                  'NEW=hard nofile 50000',
-                  'OLD2=soft nofile 4',
-                  'NEW2=soft nofile 40000'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD2/$NEW2/" $DIR',
-  path        => ['/usr/bin', '/bin'],
+  environment => ['DIR=/etc/security/limits.cong'],
+  command     => 'sudo sed -i "s/hard nofile 5/hard nofile 50000" $DIR;sudo sed -i "s/hard nofile 4/hard nofile 40000" $DIR',
+  path        => ['/bin', '/usr/bin'],
   returns     => [0, 1]
 }
